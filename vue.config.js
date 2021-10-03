83ad793807af82a03f3b53edc64cf6d493c66fe8
gcali <gcali.git@gmailcom>
2021-10-03 12:20:37 +0200

    Add pug

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+
+const TSCONFIG_PATH = path.resolve(__dirname, './tsconfig.client.json');
+
 module.exports = {
     // publicPath: process.env.NODE_ENV === "production" ? '/bowling-solitaire' : "/",
     configureWebpack: config => {
@@ -12,5 +15,27 @@ module.exports = {
             "@common": path.resolve(__dirname, "src/common"),
             "@client": path.resolve(__dirname, "src/client")
         }
+    },
+    chainWebpack: (config) => {
+        config.module
+            .rule('ts')
+            .use('ts-loader')
+            .merge({
+                options: {
+                    configFile: TSCONFIG_PATH,
+                }
+            });
+
+        config
+            .plugin('fork-ts-checker')
+            .tap(args => {
+                console.log(args[0]);
+                args[0].tsconfig = TSCONFIG_PATH;
+                if (!args[0].typescript) {
+                    args[0].typescript = {};
+                }
+                args[0].typescript.configFile = TSCONFIG_PATH;
+                return args;
+            });
     }
 }
